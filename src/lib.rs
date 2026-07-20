b328588958bd985f7f94a751410e2dc0de5c686e
reuak <87902047+reuak@users.noreply.github.com>
2026-07-20 13:44:19 +0200

    Recover placeholder OZ positions during PDF parsing

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod export;
 pub mod inline_png;
 pub mod model;
 pub mod pdf_cleanup;
+pub mod placeholder_oz;
 pub mod reference_cleanup;
 #[path = "parser_v2.rs"]
 pub mod parser;
@@ -17,6 +18,7 @@ pub use x83::{write_x83, x83_conflicts};
 pub fn parse_pdf(path: impl AsRef<Path>) -> anyhow::Result<BillOfQuantities> {
     let path = path.as_ref();
     let mut boq = parser::parse_pdf(path)?;
+    placeholder_oz::recover_placeholder_positions(path, &mut boq)?;
     reference_cleanup::repair_split_references(&mut boq);
     pdf_cleanup::postprocess_pdf(path, &mut boq)?;
     Ok(boq)
